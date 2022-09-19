0ce880661973986fd1672c092428b9fdeb7cbe81
Suryaansh2002 <suryaansh2002@gmail.com>
2022-09-19 14:21:02 +0530

    Started projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -5,6 +5,24 @@ export default function Projects() {
   return (
     <div className='projects_main'>
         <div className='projects_header' id='projecys'>My Projects</div>
+        <div className='card_c'>
+        <div className='proj_card'></div>
+        <div className='proj_card'></div>
+        <div className='proj_card'></div>
+
+        </div>
+       {/*
+       DataGrad
+       Psychup
+       Decrypt
+       Atlantis
+       Ecommerce
+       Chatify
+       Discussion room django
+       To do
+       Quiz app TS
+       Certman
+        */}
     </div>
   )
 }
