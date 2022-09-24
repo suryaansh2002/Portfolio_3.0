bc406ba681b038d92e857b13a3e56544387489f0
Suryaansh2002 <suryaansh2002@gmail.com>
2022-09-24 19:26:01 +0530

    Projects done

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Contact.css'
+import bitmoji from "../../Assets/Skills/bitmoji.png";
+
 export default function Contact() {
   return (
     <div className='contact_main'>
     <div className='contact_header' id='contact'>
     Get In Touch With Me
     </div>
+    <img src={bitmoji} className='bitmoji'/>
     </div>
+
   )
 }
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,17 +1,68 @@
-import React from 'react';
-import './Projects.css';
+import React from "react";
+import "./Projects.css";
+import psychup from "../../Assets/Skills/psych.png";
+import datagrad from "../../Assets/Skills/datagrad.png";
+import decrypt from "../../Assets/Skills/decrypt.png";
 
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
+import { HiGlobeAlt } from "react-icons/hi";
 export default function Projects() {
   return (
-    <div className='projects_main'>
-        <div className='projects_header' id='projecys'>My Projects</div>
-        <div className='card_c'>
-        <div className='proj_card'></div>
-        <div className='proj_card'></div>
-        <div className='proj_card'></div>
-
+    <div className="projects_main">
+      <div className="projects_header" id="projecys">
+        My Projects
+      </div>
+      <div className="morep"><a href="/projects">More Projects...</a></div>
+      <div className="card_c">
+        <div className="mainc">
+          <div className="proj_card">
+            <img src={psychup} className="proj_img"></img>
+            <div className="proj_title">Psychup</div>
+            <div className="proj_text">
+              It is a project aims to increase knowledge and awareness regarding mental health in India.
+            </div>
+            <div className="icons_c">
+              <AiFillGithub className="picon" />{" "}
+              <HiGlobeAlt className="picon" />{" "}
+              <AiFillInstagram className="picon" />
+            </div>
+          </div>
+          <div className="inner_card"></div>
         </div>
-       {/*
+
+        <div className="mainc">
+          <div className="proj_card">
+            <img src={datagrad} className="proj_img"></img>
+            <div className="proj_title">DataGrad</div>
+            <div className="proj_text">
+              It aims to bridge the gap between top MOOCs like Coursera,
+              and students eager to learn new skills.
+            </div>
+            <div className="icons_c2">
+              <AiFillGithub className="picon" />{" "}
+              <HiGlobeAlt className="picon" />
+            </div>
+          </div>
+          <div className="inner_card"></div>
+        </div>
+        <div className="mainc">
+          <div className="proj_card">
+            <img src={decrypt} className="proj_img"></img>
+            <div className="proj_title">Decrypt</div>
+            <div className="proj_text">
+             A website for an annual online cryptic hunt competition conducted by IEEE SBM, with over 500 participants.
+            </div>
+            <div className="icons_c2">
+              <AiFillGithub className="picon" />{" "}
+              <HiGlobeAlt className="picon" />{" "}
+            </div>
+          </div>
+          <div className="inner_card"></div>
+        </div>
+      </div>
+      {/* <div className='proj_card'></div> */}
+      {/* <div className='proj_card'></div> */}
+      {/*
        DataGrad
        Psychup
        Decrypt
@@ -24,5 +75,5 @@ export default function Projects() {
        Certman
         */}
     </div>
-  )
+  );
 }
