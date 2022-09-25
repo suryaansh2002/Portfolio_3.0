a7e36acf7a091243d54f73345fa84cbe9c303cbb
Suryaansh2002 <suryaansh2002@gmail.com>
2022-09-25 06:14:40 +0530

    Links added + projectPage

--- src/Components/ProjectsPage/ProjectsPage.jsx
+++ src/Components/ProjectsPage/ProjectsPage.jsx
@@ -1,21 +1,23 @@
-import React from "react";
-import "./Projects.css";
+import React from 'react'
+import "./ProjectsPage.css";
 import psychup from "../../Assets/Skills/psych.png";
 import datagrad from "../../Assets/Skills/datagrad.png";
 import decrypt from "../../Assets/Skills/decrypt.png";
+import atlantis from "../../Assets/Skills/atlantis.png";
 
 import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 import { HiGlobeAlt } from "react-icons/hi";
-export default function Projects() {
+
+export default function ProjectsPage() {
   return (
-    <div className="projects_main">
-      <div className="projects_header" id="projects">
-        My Projects
-      </div>
-      <div className="morep"><a href="/projects">More Projects...</a></div>
-      <div className="card_c">
-          {/*Psychup */}
-          <div className="mainc">
+    <div className="projectsPage_main">
+    <div className="projectsPage_header">
+      All Projects
+    </div>
+    <div className="card_c">
+      
+      {/*Psychup */}
+      <div className="mainc">
         <div className="proj_card">
           <img src={psychup} className="proj_img"></img>
           <div className="proj_title">Psychup</div>
@@ -65,19 +67,54 @@ export default function Projects() {
         </div>
         <div className="inner_card"></div>
       </div>
+
+      {/*Atlantis */}
+      <div className="mainc">
+        <div className="proj_card">
+          <img src={atlantis} className="proj_img"></img>
+          <div className="proj_title">Atlantis</div>
+          <div className="proj_text">
+           A website for an annual online cryptic hunt competition conducted by IEEE SBM, with over 500 participants.
+          </div>
+          <div className="icons_c2">
+            {/* <AiFillGithub className="picon" />{" "} */}
+            <a href='https://atlantis.ieeemanipal.com/'><HiGlobeAlt className="picon" />{" "}</a>
+          </div>
+        </div>
+        <div className="inner_card"></div>
       </div>
-      {/*
-       DataGrad
-       Psychup
-       Decrypt
-       Atlantis
-       Ecommerce
-       Chatify
-       Discussion room django
-       To do
-       Quiz app TS
-       Certman
-        */}
+
+      {/*Ecommerce */}
+      <div className="mainc">
+        <div className="proj_card">
+          <img src={atlantis} className="proj_img"></img>
+          <div className="proj_title">Ecommerce website</div>
+          <div className="proj_text">
+          A basic Ecommerce website built using MongoDB, Express, React JS, and Node JS. Styled using bootstrap.
+          </div>
+          <div className="icons_c2">
+            <a href='https://github.com/suryaansh2002/Ecommerce_site_practice'><AiFillGithub className="picon" /></a>
+            <a href='https://ecommerce.suryaanshrathinam.com/'><HiGlobeAlt className="picon" />{" "}</a>
+          </div>
+        </div>
+        <div className="inner_card"></div>
+      </div>
+
     </div>
-  );
+
+    {/*
+     DataGrad
+     Psychup
+     Decrypt
+     Atlantis
+     Ecommerce
+     Chatify
+     Discussion room django
+     To do
+     Quiz app TS
+     Certman
+      */}
+  </div>
+
+  )
 }
--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import Home from './Components/Home/Home';
 import Main from './Components/Main/Main';
 import SkillsPage from './Components/SkillsPage/SkillsPage';
 import Landing from './Components/Landing/Landing';
+import ProjectsPage from './Components/ProjectsPage/ProjectsPage';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root")
@@ -22,6 +23,8 @@ root.render(
     <Routes>
       <Route path="/" element={<Main/>}/>
       <Route path='skills' element={<SkillsPage/>}/>
+      <Route path='projects' element={<ProjectsPage/>}/>
+
         {/* <Route path="teams" element={<Teams />}>
           <Route path=":teamId" element={<Team />} />
           <Route path="new" element={<NewTeamForm />} />
